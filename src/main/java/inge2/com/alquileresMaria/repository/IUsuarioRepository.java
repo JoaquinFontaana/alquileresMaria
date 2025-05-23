d5616b2e2858e50eaf0b73f6ef439a74c1850d46
joaquinfontana <fontana200514@gmail.com>
2025-05-23 08:33:52 -0300

    esquema logico de iniciar sesion

---
+++ src/main/java/inge2/com/alquileresMaria/repository/IUsuarioRepository.java
@@ -0,0 +1,10 @@
+package inge2.com.alquileresMaria.repository;
+
+import inge2.com.alquileresMaria.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IUsuarioRepository extends JpaRepository<Usuario,Long> {
+    public Optional<Usuario> findByMail(String mail);
+}
---
+++ src/main/java/inge2/com/alquileresMaria/service/UsuarioService.java
@@ -0,0 +1,26 @@
+package inge2.com.alquileresMaria.service;
+
+import inge2.com.alquileresMaria.model.Usuario;
+import inge2.com.alquileresMaria.repository.IUsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+    @Autowired
+    private EncryptService encryptService;
+
+    public String iniciarSesion(String mail,String password){
+        Usuario usuario = this.usuarioRepository.findByMail(mail)
+                .orElseThrow(()-> new EntityNotFoundException("El mail no existe o la contraseña ingresada no coincide "));
+        if(!this.encryptService.verifyPassword(password,usuario.getPassword())){
+            //Contraseña incorrecta
+        }
+        String nombreRol = usuario.getRol().getNombre();
+        //Buscar segun el rol en la tabla empleado o cliente
+        return "Hola";
+    }
+}
