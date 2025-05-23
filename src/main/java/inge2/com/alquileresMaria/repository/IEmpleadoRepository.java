334a6ae89a3046455cd1bbc88f724d957fd7eada
tomas <tomasbernardi03@gmail.com>
2025-05-23 12:05:06 -0300

    Actualizar auto

--- src/main/java/inge2/com/alquileresMaria/repository/IEmpleadoRepository.java
+++ src/main/java/inge2/com/alquileresMaria/repository/IEmpleadoRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IEmpleadoRepository extends JpaRepository<Empleado,Long>{
     boolean existsByMail(String mail);
-    boolean existsByDNI(String DNI);
 }
