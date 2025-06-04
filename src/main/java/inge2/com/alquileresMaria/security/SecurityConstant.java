0ddda7659c2a2e09948803d57dfc2d3ed871dffa
tomas <tomasbernardi03@gmail.com>
2025-06-04 11:34:50 -0300

    Expiracion alargada

--- src/main/java/inge2/com/alquileresMaria/security/SecurityConstant.java
+++ src/main/java/inge2/com/alquileresMaria/security/SecurityConstant.java
@@ -1,6 +1,6 @@
 package inge2.com.alquileresMaria.security;
 
 public class SecurityConstant {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 7000000;
     public static final String JWT_SECRET = "alquileresMariaNecesita32CaracteresYNOSEQUEMASPONER123";
 }
