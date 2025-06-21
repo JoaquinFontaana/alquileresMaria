b86f6794d3401370030d2a17196df57eb29a9a8b
joaquinfontana <fontana200514@gmail.com>
2025-06-20 21:35:31 -0300

    pasa a admin controller

--- src/main/java/inge2/com/alquileresMaria/controller/AdminController.java
+++ src/main/java/inge2/com/alquileresMaria/controller/AdminController.java
@@ -1,10 +1,25 @@
 package inge2.com.alquileresMaria.controller;
 
+import inge2.com.alquileresMaria.dto.user.EmpleadoDTO;
+import inge2.com.alquileresMaria.service.EmpleadoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private final EmpleadoService empleadoService;
 
+    public AdminController(EmpleadoService empleadoService) {
+        this.empleadoService = empleadoService;
+    }
+
+    @PostMapping(path = "/empleado/registrar")
+    public ResponseEntity<String> crearEmpleado(EmpleadoDTO empleadoDTO){
+        this.empleadoService.crearEmpleado(empleadoDTO);
+        return new ResponseEntity<String>("Empleado registrado con exito", HttpStatus.OK);
+    }
 }
--- src/main/java/inge2/com/alquileresMaria/controller/EmpleadoController.java
+++ src/main/java/inge2/com/alquileresMaria/controller/EmpleadoController.java
@@ -12,15 +12,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/empleado")
 public class EmpleadoController {
 
-    private final EmpleadoService empleadoService;
 
-    public EmpleadoController(EmpleadoService empleadoService) {
-        this.empleadoService = empleadoService;
-    }
-
-    @PostMapping(path = "/registrar")
-    public ResponseEntity<String> crearEmpleado(EmpleadoDTO empleadoDTO){
-        this.empleadoService.crearEmpleado(empleadoDTO);
-        return new ResponseEntity<String>("Empleado registrado con exito",HttpStatus.OK);
-    }
 }
